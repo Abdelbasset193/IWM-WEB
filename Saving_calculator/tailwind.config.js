c303377e46adcdcce5563e9b8b4d830bae39f307
Abdelbasset193 <104647443+Abdelbasset193@users.noreply.github.com>
2022-11-14 02:13:03 +0100

    first_interface_design_talwind_flowbitejs

--- Saving_calculator/tailwind.config.js
+++ Saving_calculator/tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  //content: ["./index.html" , "./node_modules/flowbite/**/*.js"
+  content: ['index.html','./src/**/*.html', './node_modules/flowbite/**/*.js'],
+
+
+
   theme: {
     extend: {},
+    colors:{
+      'primary' :'#F5F5F7',
+      'secondary': '#34C648',
+      'title': '#7C8899',
+      'byad': '#FFF',
+      'ktba': '#000',
+      'limouni':'#F6A828'
+    },
+    margin:{
+     'sm' : '8px',
+     'md': '16px',
+       'lg': '24px',
+       'xl': '48px',
+    },
+    padding:{
+      'sm' : '8px',
+      'md': '16px',
+        'lg': '24px',
+        'xl': '48px',
+     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+
+  ],
 }
